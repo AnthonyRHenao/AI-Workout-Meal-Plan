a5563e5ac680735302865ebded3e3201ea50abc3
Zino Ebiala <eebia002@fiu.edu>
2023-09-06 13:56:56 -0400

    added font awesome script

---
+++ mealplan/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, {useState, useEffect} from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+        <div>
+            <Link>
+            
+            </Link>
+            <div>
+                
+            </div>
+        </div>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
