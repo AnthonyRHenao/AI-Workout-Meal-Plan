104da767bab573fb8a4b300f0d9503e3f9b1a827
Mark Santander <125848110+marksantander@users.noreply.github.com>
2023-09-18 19:40:03 -0400

    Delete mealplan directory

--- mealplan/server/models/index.js
+++
@@ -1,6 +0,0 @@
-const User = require('./User');
-const Product = require('./Product');
-const Category = require('./Category');
-const MealPlan = require('./MealPlan');
-
-module.exports = { User, Product, Category, Order };
